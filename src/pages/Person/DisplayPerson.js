011945e97ce5c7f8647194a4d5fef42bae63f0e8
Arjun K Prasad <arjun1237.kp@gmail.com>
2021-05-29 18:45:05 +0530

    home button added

--- src/pages/Person/DisplayPerson.js
+++ src/pages/Person/DisplayPerson.js
@@ -3,16 +3,27 @@ import styles from './index.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMale, faFemale } from '@fortawesome/free-solid-svg-icons'
 import { Bio } from './Bio';
+import { useHistory } from 'react-router';
 
 export const DisplayPerson = ({person, homeWorld, films}) => {
+	const history = useHistory()
+
+	const clickHandler = () => {
+		history.push('/')
+	}
 
 	return (
-		<div className={styles.personDetails + " color-yellow"}>
-			<div className={styles.frame + " border-color-yellow"}>
-				<h1>{person.name}</h1>
-				<FontAwesomeIcon icon={person.gender === "male" ? faMale : faFemale} size="10x" />
+		<div>
+			<div className={styles.personDetails + " color-yellow"}>
+				<div className={styles.frame + " border-color-yellow"}>
+					<h1>{person.name}</h1>
+					<FontAwesomeIcon icon={person.gender === "male" ? faMale : faFemale} size="10x" />
+				</div>
+				<Bio person={person} homeWorld={homeWorld} films={films} />
 			</div>
-			<Bio person={person} homeWorld={homeWorld} films={films} />
+			<div className={styles.btnWrap}>
+				<div className={"border-color-yellow color-yellow " + styles.btnHover} onClick={clickHandler}>Back to HOME</div>
+			</div>
 		</div>
 	)
 }
