b7d31fb771e66cf14c72469b336e400f135bd116
Arjun K Prasad <arjun1237.kp@gmail.com>
2021-05-29 15:57:49 +0530

    some more refactoring

--- src/pages/Person/index.js
+++ src/pages/Person/index.js
@@ -4,26 +4,38 @@ import {getPerson} from '../../utils/api-calls'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { DisplayPerson } from './DisplayPerson';
+import { getFilms, getHomeWorld } from '../../utils/api-calls';
 
 function Person() {
   let {id} = useParams()
   const [person, setPerson] = useState(null)
   const [loading, setLoading] = useState(false)
+	let [homeWorld, setHomeWorld] = useState("")
+	let [films, setFilms] = useState([])
 
   useEffect(() => {
     (async function(){
       setLoading(true)
-      const res = await getPerson(id)
-      if(res){
-        setPerson(res)
+      const person = await getPerson(id)
+      if(person){
+        setPerson(person)
       }
+			let home = await getHomeWorld(person.homeworld)
+			if(home){
+				setHomeWorld(home.name)
+			}
+
+			let films = await getFilms(person.films)
+			if(films){
+				setFilms(films)
+			}
       setLoading(false)
     })()
 
-  }, [id])
+  }, [])
 
   return  loading ? <FontAwesomeIcon icon={faSpinner} size="6x" className={`color-yellow spinner`} /> : 
-          person ?  <DisplayPerson person={person} /> : <h1 className="color-yellow">Person Not Found..!</h1>;
+          person ?  <DisplayPerson person={person} homeWorld={homeWorld} films={films} /> : <h1 className="color-yellow">Person Not Found..!</h1>;
 }
 
 export default Person;
